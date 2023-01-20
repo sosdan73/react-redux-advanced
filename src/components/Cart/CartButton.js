ef976ec8bad0d75e11440a9ecd7bfc56870a9398
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-20 11:04:12 +0200

    adds cart toggle dispatch and handler function

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,18 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
+	const dispatch = useDispatch();
+	const handleToggleCart = () => {
+		dispatch(uiActions.toggle());
+	}
+
 	return (
-		<button className={classes.button}>
+		<button
+			className={classes.button}
+			onClick={handleToggleCart}
+		>
 			<span>My Cart</span>
 			<span className={classes.badge}>1</span>
 		</button>
