2a6aaa3cd8b7e22ee6def7cf41e587f098fb0f6d
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-20 17:53:22 +0200

    1) adds notification component on the page 2) adds `dispatch` method to the useEffect dependencies 3) stops useEffect from running when the component is mounted 4) adds action creator in the cart slice, ran in the useEffect

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {uiActions} from "./ui-slice";
 
 const initialState = {
 	items: [],
@@ -38,7 +39,44 @@ const cartSlice = createSlice({
 			state.items = state.items.filter(item => item.id !== id);
 		},
 	}
-})
+});
 
+export const sendCartData = (cart) => {
+	return async (dispatch) => {
+		dispatch(uiActions.showNotification({
+			status: 'pending',
+			title: 'Sending...',
+			message: 'Sending cart data',
+		}))
+
+		const sendRequest = async () => {
+			const response = await fetch(
+				'https://react-redux-99c7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+					method: 'PUT',
+					body: JSON.stringify(cart),
+				});
+			if (!response.ok) {
+				throw new Error('Sending cart data failed')
+			}
+			return response
+		}
+
+		try {
+			await sendRequest();
+			dispatch(uiActions.showNotification({
+				status: 'success',
+				title: 'Success...',
+				message: 'Sent data successfully',
+			}))
+		} catch (error) {
+			dispatch(uiActions.showNotification({
+				status: 'error',
+				title: 'Error!',
+				message: 'Sending cart data failed',
+			}))
+		}
+
+	}
+}
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
