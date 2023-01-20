d455b12e3241deb193127bf063483080943c2b05
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-20 13:16:17 +0200

    adds Dummy products, mapping through them in the Products component and handling addition to the cart in ProductItem component

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,14 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../store/cart-slice";
 
-const ProductItem = (props) => {
-	const { title, price, description } = props;
+const ProductItem = ({title, price, description, id}) => {
+	const dispatch = useDispatch()
+
+	const handleAddToCart = () => {
+		dispatch(cartActions.addItem({title, price, id}))
+	}
 
 	return (
 		<li className={classes.item}>
@@ -13,7 +19,7 @@ const ProductItem = (props) => {
 				</header>
 				<p>{description}</p>
 				<div className={classes.actions}>
-					<button>Add to Cart</button>
+					<button onClick={handleAddToCart}>Add to Cart</button>
 				</div>
 			</Card>
 		</li>
